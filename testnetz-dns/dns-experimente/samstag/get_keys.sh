d9de136f0187c253d1b2de72630cb59a8c93f24b
Paul Weiss <paul@paul.de>
2017-09-14 17:31:40 +0200

    samstag add

---
+++ testnetz-dns/dns-experimente/samstag/get_keys.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Start der Generierung"
+date
+echo ""
+dnssec-keygen -a RSASHA1 -b 1024 -n ZONE de
+
+echo "ZSK key done"
+echo ""
+date
+echo ""
+echo ""
+dnssec-keygen -a RSASHA1 -b 2048 -f KSK -n ZONE de
+echo ""
+echo "KSK key done"
+echo ""
+date
+
+echo "Ende"
+
+
+
+
+
+
+
+
+

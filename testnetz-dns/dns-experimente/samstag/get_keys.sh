a2db0fdbd2dff2c6b54a7a66b501d3438a40f359
Paul Weiss <paul@paul.de>
2017-09-20 17:21:07 +0200

    add keys 008-RSASHA256

--- testnetz-dns/dns-experimente/samstag/get_keys.sh
+++ testnetz-dns/dns-experimente/samstag/get_keys.sh
@@ -3,14 +3,14 @@
 echo "Start der Generierung"
 date
 echo ""
-dnssec-keygen -a RSASHA1 -b 1024 -n ZONE de
+dnssec-keygen -a RSASHA256 -b 1024 -n ZONE de
 
 echo "ZSK key done"
 echo ""
 date
 echo ""
 echo ""
-dnssec-keygen -a RSASHA1 -b 2048 -f KSK -n ZONE de
+dnssec-keygen -a RSASHA256 -b 2048 -f KSK -n ZONE de
 echo ""
 echo "KSK key done"
 echo ""
